3c594db53e7c1bbea301df08149c70cb12a44041
wanhui <13658247573@163.com>
2025-07-10 15:52:30 +0800

    update: add benchmark

--- test_api.py
+++ test_api.py
@@ -4,11 +4,11 @@ import base64
 def test_zero_shot():
     payload = {
         "spk_id": "a25c604e-f40d-4060-9d9d-3ae7b3864791",
-        "tts_text": '唐杰明天来上海出差，我带你去上海滩浪一浪。'
+        "tts_text": """你好, 我是一个测试文本, 请你帮我合成语音。"""
     }
 
     resp = requests.post(
-        'http://127.0.0.1:50000/inference_zero_shot',
+        'http://10.24.8.90:50000/inference_zero_shot',
         data=payload
     )
 
